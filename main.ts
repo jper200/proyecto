5aa4e7cb84a87c9800a3c77973afe230cdd56ed7
jper200 <112582736+jper200@users.noreply.github.com>
2022-11-09 11:44:50 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,4 +7,20 @@ input.onButtonPressed(Button.B, function () {
 let player_sprite: game.LedSprite = null
 player_sprite = game.createSprite(2, 5)
 basic.pause(5000)
-let enemy_1_sprite = game.createSprite(randint(1, 5), 0)
+let block_1 = game.createSprite(randint(0, 4), 0)
+basic.pause(1000)
+basic.forever(function () {
+    if (block_1.isTouching(player_sprite)) {
+        basic.pause(500)
+        block_1.delete()
+        block_1 = game.createSprite(2, 2)
+    } else {
+        block_1.change(LedSpriteProperty.Y, 1)
+        basic.pause(1000)
+    }
+    if (block_1.get(LedSpriteProperty.Y) == 4) {
+        basic.pause(500)
+        block_1.delete()
+        game.gameOver()
+    }
+})
