de6576a79c710baa254e2e220c25664e42e2925d
jper200 <112582736+jper200@users.noreply.github.com>
2022-11-10 06:47:37 -0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,33 +5,34 @@ input.onButtonPressed(Button.B, function () {
     player_sprite.change(LedSpriteProperty.X, 1)
 })
 let player_sprite: game.LedSprite = null
-game.setScore(0)
 player_sprite = game.createSprite(2, 5)
 basic.pause(3000)
 let block_1 = game.createSprite(randint(0, 4), 0)
 basic.pause(1000)
 let enemy_1 = 1
 basic.forever(function () {
-    if (block_1.isTouching(player_sprite)) {
-        enemy_1 = 0
-        block_1.delete()
-        block_1 = game.createSprite(player_sprite.get(LedSpriteProperty.X), 3)
-    }
-    while (block_1.get(LedSpriteProperty.Y) == 4) {
-        basic.pause(200)
-        if (block_1.get(LedSpriteProperty.Y) == 4) {
-            basic.pause(100)
+    while (enemy_1 == 0) {
+        if (block_1.get(LedSpriteProperty.X) != player_sprite.get(LedSpriteProperty.X)) {
             block_1.delete()
-            game.gameOver()
+            block_1 = game.createSprite(player_sprite.get(LedSpriteProperty.X), 3)
+        } else {
+        	
         }
     }
-    if (enemy_1 == 1) {
-        block_1.change(LedSpriteProperty.Y, 1)
-        basic.pause(1000)
-    }
-    if (enemy_1 == 0) {
-        block_1.delete()
-        block_1 = game.createSprite(player_sprite.get(LedSpriteProperty.X), 3)
-        game.addScore(1)
+    while (enemy_1 == 1) {
+        if (block_1.get(LedSpriteProperty.X) == player_sprite.get(LedSpriteProperty.X) && block_1.get(LedSpriteProperty.Y) == 3) {
+            enemy_1 = 0
+        } else {
+            block_1.change(LedSpriteProperty.Y, 1)
+            basic.pause(1000)
+        }
+        while (block_1.get(LedSpriteProperty.Y) == 4) {
+            basic.pause(350)
+            if (block_1.get(LedSpriteProperty.Y) == 4) {
+                basic.pause(100)
+                block_1.delete()
+                game.gameOver()
+            }
+        }
     }
 })
